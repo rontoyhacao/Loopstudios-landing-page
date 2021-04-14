b3a27ea72c7e571dba1ce67498fe8e7d540575cf
Ron Paolo Toyhacao <rptoyhacao@gmail.com>
2021-04-15 01:07:12 +0800

    Mobile design added

---
+++ assets/scripts/_hamburger.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger-btn');
+// const hamburgerNav = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    // hamburgerNav.classList.toggle('show');
+    hamburger.classList.toggle('open');
+});
